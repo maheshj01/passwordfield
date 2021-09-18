08aa4845e9e9a7d07ee856b878b682c0c7d6a1e5
mahesh jamdade <maheshmn121@gmail.com>
2021-09-18 22:35:23 +0530

    remove todo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,8 +25,6 @@ class _MyAppState extends State<MyApp> {
 }
 
 class PasswordApp extends StatelessWidget {
-  /// TODO: Add examples in readme with gifs
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
